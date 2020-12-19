dded54da928c20efee739f0b04d02254c35d430c
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-19 14:43:17 -0500

    Finished the email_address validation

--- lib/domain/auth/email_address.dart
+++ lib/domain/auth/email_address.dart
@@ -1,11 +1,13 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'email_address.freezed.dart';
 
 class EmailAddress {
-  final String value;
-  final Either<Left, Right> failure;
+  // values data type can be Either a ValueFailure or a String
+  // the left side of an either is always the fail case
+  final Either<ValueFailure<String>, String> value;
 
   factory EmailAddress(String input) {
     assert(input != null);
@@ -13,7 +15,7 @@ class EmailAddress {
     return EmailAddress._(value: validateEmailAddress(input));
   }
 
-  const EmailAddress._({this.value, this.failure});
+  const EmailAddress._({this.value});
 
   @override
   String toString() {
@@ -30,21 +32,37 @@ class EmailAddress {
 }
 
 // The validation is done here instead of the common Text form field
-String validateEmailAddress(String input) {
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   // Maybe not the most robust way of email validation but it's good enough
   const emailRegex = r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
   if (RegExp(emailRegex).hasMatch(input)) {
-    // input is valid
-    return input;
+    // input is valid, email address is correct
+    return right(input);
   } else {
     // if input is bad, throw an exception
-    throw InvalidEmailException(failedValue: input);
+    return left(ValueFailure.invalidEmail(failedValue: input));
   }
 }
 
+// The different types of passwords when authenticating
+// type in the terminal flutter pub run build_runner watch to generate the code from freezed
+// watch keyword is so that if changes are made in my code, the generator automatically generates the changes
 @freezed
 abstract class ValueFailure<T> with _$ValueFailure<T> {
   const factory ValueFailure.invalidEmail({
     @required String failedValue,
   }) = InvalidEmail<T>;
+  const factory ValueFailure.shortPassword({
+    @required String failedValue,
+  }) = ShortPassword<T>;
+}
+
+void showTheEmailAddressOrFailure() {
+  final emailAddress = EmailAddress('fafsa');
+
+  String emailText = emailAddress.value.fold((left) => 'Failure happened, $left', (right) => right);
+
+  // This does the same thing as using the fold method
+  // Difference is that I don't get access to the left variable.
+  String emailText2 = emailAddress.value.getOrElse(() => 'Some failure happened');
 }

d1ca126bfe27f61020b49e2a8f5b3a498b30ad15
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-27 16:45:06 -0500

    Completed the sign_in_form_state bloc

--- lib/application/auth/sign_in_form/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ddd_to_do/domain/auth/auth_failure.dart';
+import 'package:ddd_to_do/domain/auth/i_auth_facade.dart';
+import 'package:ddd_to_do/domain/auth/value_objects.dart';
 import 'package:meta/meta.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -10,7 +14,9 @@ part 'sign_in_form_state.dart';
 part 'sign_in_form_bloc.freezed.dart';
 
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
-  SignInFormBloc() : super(SignInFormInitial());
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
 
   @override
   Stream<SignInFormState> mapEventToState(
--- lib/application/auth/sign_in_form/sign_in_form_state.dart
+++ lib/application/auth/sign_in_form/sign_in_form_state.dart
@@ -1,6 +1,20 @@
 part of 'sign_in_form_bloc.dart';
 
-@immutable
-abstract class SignInFormState {}
+// Think of the possible states for auth
 
-class SignInFormInitial extends SignInFormState {}
+@freezed
+abstract class SignInFormState with _$SignInFormState {
+  const factory SignInFormState(
+      {@required EmailAddress emailAddress,
+      @required Password password,
+      @required bool showErrorMessages,
+      @required bool isSubmitting,
+      @required Option<Either<AuthFailure, Unit>> authFailureOrSuccessOption}) = _SignInFormState;
+
+  factory SignInFormState.initial() => SignInFormState(
+      emailAddress: EmailAddress(''),
+      password: Password(''),
+      showErrorMessages: false,
+      isSubmitting: false,
+      authFailureOrSuccessOption: none());
+}

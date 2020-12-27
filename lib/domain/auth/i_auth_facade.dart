6ba04d239c09c29fbc67b89f69b39b9993e1e43c
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-26 20:07:34 -0500

    Completed the IAuthFacade with proper failures

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,3 +1,5 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_to_do/domain/auth/auth_failure.dart';
 import 'package:ddd_to_do/domain/auth/value_objects.dart';
 import 'package:flutter/foundation.dart';
 
@@ -5,14 +7,17 @@ import 'package:flutter/foundation.dart';
 // weird interfaces into just one simplified interface. In our case, it will connect
 // FirebaseAuth and GoogleSignIn.
 
+// This interface allows us to implement the application logic without any FirebaseAuth dependencies!!!
 abstract class IAuthFacade {
-  Future<void> registerWithEmailAndPassword({
+  // Use Unit here instead of void. This is because Unit is an actual class
+  // whereas void is just a keyword !!!
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
     @required EmailAddress emailAddress,
     @required Password password,
   });
-  Future<void> signInWithEmailAndPassword({
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
     @required EmailAddress emailAddress,
     @required Password password,
   });
-  Future<void> signInWithGoogle();
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }

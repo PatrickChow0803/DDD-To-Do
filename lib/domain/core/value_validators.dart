9760e9ac795f9b6f76ca1322bde5be455e001115
patrickchow0803 <patrickchow0803@gmail.com>
2021-01-06 17:12:41 -0500

    Added the value validators for notes

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -2,6 +2,51 @@
 import 'package:dartz/dartz.dart';
 import 'package:ddd_to_do/domain/auth/value_objects.dart';
 import 'package:ddd_to_do/domain/core/failures.dart';
+import 'package:kt_dart/collection.dart';
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(
+      // used to display to the user what went wrong
+      ValueFailure.exceedingLength(failedValue: input, max: maxLength),
+    );
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (!input.contains('\n')) {
+    return right(input);
+  } else {
+    return left(ValueFailure.multiline(failedValue: input));
+  }
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+  KtList<T> input,
+  int maxLength,
+) {
+  if (input.size <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.listTooLong(
+      failedValue: input,
+      max: maxLength,
+    ));
+  }
+}
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   // Maybe not the most robust way of email validation but it's good enough

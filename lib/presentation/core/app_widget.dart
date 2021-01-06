684ccfc0f5e434c7e65c4b0dc9f2dc15624caf81
patrickchow0803 <patrickchow0803@gmail.com>
2021-01-06 12:29:12 -0500

    Added auto routes

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,19 +1,33 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:ddd_to_do/application/auth/auth_bloc.dart';
+import 'package:ddd_to_do/injection.dart';
+import 'package:ddd_to_do/presentation/routes/router.gr.dart';
 import 'package:ddd_to_do/presentation/sign_in/sign_in_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notes',
-      debugShowCheckedModeBanner: false,
-      home: SignInPage(),
-      theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.teal[800],
-          // this creates the border around TextInputFields. Ex: The email and password
-          inputDecorationTheme: InputDecorationTheme(
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)))),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()))
+      ],
+      child: MaterialApp(
+        title: 'Notes',
+        debugShowCheckedModeBanner: false,
+        // home: SignInPage(),
+        builder: ExtendedNavigator(
+          router: MyRouter(),
+        ),
+        theme: ThemeData.dark().copyWith(
+            primaryColor: Colors.teal[800],
+            // this creates the border around TextInputFields. Ex: The email and password
+            inputDecorationTheme: InputDecorationTheme(
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)))),
+      ),
     );
   }
 }

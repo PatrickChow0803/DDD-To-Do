c7bbaf4d3e61747466ac9a9b46220f3410e80883
patrickchow0803 <patrickchow0803@gmail.com>
2021-01-09 10:20:41 -0500

    Added the watchAll method in note_repository.dart

--- lib/infrastructure/core/firestore_helpers.dart
+++ lib/infrastructure/core/firestore_helpers.dart
@@ -5,11 +5,12 @@ import 'package:ddd_to_do/injection.dart';
 
 // File contains extension methods for Firestore related code
 
+// users/{userId}/notes/{notesId}
 extension FirestoreX on FirebaseFirestore {
   Future<DocumentReference> userDocument() async {
     final userOption = await getIt<IAuthFacade>().getSignedInUser();
     final user = userOption.getOrElse(() => throw NotAuthenticatedError());
-    return Firestore.instance.collection('users').document(user.id.getOrCrash());
+    return FirebaseFirestore.instance.collection('users').doc(user.id.getOrCrash());
   }
 }
 
--- lib/infrastructure/notes/note_repository.dart
+++ lib/infrastructure/notes/note_repository.dart
@@ -7,6 +7,9 @@ import 'package:ddd_to_do/domain/notes/note_failure.dart';
 import 'package:injectable/injectable.dart';
 import 'package:kt_dart/kt.dart';
 import 'package:ddd_to_do/infrastructure/core/firestore_helpers.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'note_dtos.dart';
 
 @LazySingleton(as: INoteRepository)
 class NoteRepository implements INoteRepository {
@@ -16,7 +19,35 @@ class NoteRepository implements INoteRepository {
 
   // async* here because return type is a Stream, not a Future
   @override
-  Stream<Either<NoteFailure, KtList<Note>>> watchAll() async* {}
+  Stream<Either<NoteFailure, KtList<Note>>> watchAll() async* {
+    // need to manually import extension method from firestore_helpers
+    final userDoc = await _firestore.userDocument();
+    // note_dto has a field called serverTimeStamp,
+    // we want to order the documents in which they come in by serverTimeStamp,
+    // aka: The last note that was interacted with
+    // yield* because .snapshots() returns a stream
+    // the first .map is to get the correct data from snapshots
+    // the second .map is to convert QuerySnapshot from .snapshots() to
+    // Stream<Either<NoteFailure, KtList<Note>>>
+    yield* userDoc.noteCollection
+        .orderBy('serverTimeStamp', descending: true)
+        .snapshots()
+        .map(
+          (snapshot) => right<NoteFailure, KtList<Note>>(
+            snapshot.docs.map((doc) => NoteDto.fromFirestore(doc).toDomain()).toImmutableList(),
+          ),
+          // need to manually import rxdart to use this extension method
+          // this method is called when .map() throws an exception
+        )
+        .onErrorReturnWith((error) {
+      if (error is FirebaseException && error.message.contains('PERMISSION_DENIED')) {
+        return left(const NoteFailure.insufficientPermission());
+      } else {
+        // log.error(e.toString());
+        return left(const NoteFailure.unexpected());
+      }
+    });
+  }
 
   @override
   Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted() async* {}
